f1b126ade9978b3e5c2eab5fc5226580fc905fb6
TulioAvanco <tulioavanco12@gmail.com>
2021-07-29 16:30:58 -0300

    Adicionado paginas desafio 1 - 2 - 3 component navigator e configurado rotas

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,21 @@
+import { Desafio3Component } from './component/desafio3/desafio3.component';
+import { Desafio2Component } from './component/desafio2/desafio2.component';
+import { Desafio1Component } from './component/desafio1/desafio1.component';
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: "",
+  component: Desafio1Component },
+{
+  path:"desafio2",
+  component: Desafio2Component
+},
+{
+  path:"desafio3",
+  component:Desafio3Component
+}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
